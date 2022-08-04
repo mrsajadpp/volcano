395770e05ef75898b99d6f34a6cf4afe757d375a
Sajad pp <96676224+mrsajadpp@users.noreply.github.com>
2022-08-04 19:43:40 +0530

    Create badword.js

---
+++ detector/badword.js
@@ -0,0 +1,36 @@
+function remove (client, message,badWords,say,roleId,chId) {
+  var i = 0;
+  var word = '';
+  var count = 0;
+  for ( i = 0; i < badWords.length; i++) {
+    if (badWords[i].includes(message.content)) {
+       word = badWords[i];
+    }
+  }
+  if (badWords.includes(message.content)) {
+    var msg = message;
+    for (var l = 0; l < say.length; l++) {
+      if (say[l].id === message.author.id) {
+        say[l] = {
+           id: message.author.id,
+           count: say[l].count + 1
+        };
+        if (say[l].count == 5) {
+            let role = message.guild.roles.cache.find(r => r.name === "muted");
+          message.member.roles.add(role).catch(console.error);
+          say[l].count = 0;
+          role = '';
+        }
+      } else {
+        say.push({
+           id: message.author.id,
+           count: count + 1
+        });
+        count = 0;
+      }
+    }
+    message.delete();
+    message.channel.send(`🩹 Removed <@${message.author.id}>'s Message as it had a bad word in it used %**#*. Bad word's strictly prohibited this server.`).catch(console.error);
+  }
+}
+module.exports = remove;
